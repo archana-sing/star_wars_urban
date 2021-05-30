f2007b84ab51583edcfa8492ce7b2fe09c1546ec
archana singh <archanasingh1643@gmail.com>
2021-05-30 14:16:57 +0530

    Updates

---
+++ precache-manifest.cd7768be6286dff114fd59f394cb0762.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7149226b8fcdb0b549b4e302fe92c17a",
+    "url": "/star_wars_urban/index.html"
+  },
+  {
+    "revision": "f63b5fa9a7288b2e0561",
+    "url": "/star_wars_urban/static/css/main.ceb22f43.chunk.css"
+  },
+  {
+    "revision": "408c1cf0b5bb97e7e331",
+    "url": "/star_wars_urban/static/js/2.383dc80a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/star_wars_urban/static/js/2.383dc80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63b5fa9a7288b2e0561",
+    "url": "/star_wars_urban/static/js/main.ceb2322a.chunk.js"
+  },
+  {
+    "revision": "24a795f3cbdddd995fb7",
+    "url": "/star_wars_urban/static/js/runtime-main.04183d31.js"
+  },
+  {
+    "revision": "c79f33c6a15c178b58e8b943bc550f72",
+    "url": "/star_wars_urban/static/media/loading-gif-orange-10.c79f33c6.gif"
+  },
+  {
+    "revision": "bdbed4d83378d2b7cebdd92a457dd447",
+    "url": "/star_wars_urban/static/media/search-button.bdbed4d8.png"
+  },
+  {
+    "revision": "ac40f9b7bed3a9767ea9772c33392c38",
+    "url": "/star_wars_urban/static/media/star_wars.ac40f9b7.jpg"
+  }
+]);
